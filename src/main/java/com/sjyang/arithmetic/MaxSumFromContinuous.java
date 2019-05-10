fe202167fcce0433ce0f1a4c1736c30ae3fb6bbb
xQuantComputer <511574545@qq.com>
2019-05-10 11:09:40 +0800

    分治算法-求最大子列和

--- src/main/java/com/sjyang/arithmetic/MaxSumFromContinuous.java
+++ src/main/java/com/sjyang/arithmetic/MaxSumFromContinuous.java
@@ -12,8 +12,9 @@ package com.sjyang.arithmetic;
  */
 public class MaxSumFromContinuous {
     public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println(maxSubSum3(arr,0,8));
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5};
+        System.out.println(divideAndConque(arr,0,7));
+        System.out.println(maxSubSum4(arr));
     }
 
     /**
@@ -78,8 +79,9 @@ public class MaxSumFromContinuous {
     /**
      * 分治（分而治之）
      * @param a 处理数组
-     * @param left 左边界
-     * @param right 右边界
+     * @param left 左边界0
+     * @param right 右边界7
+     * 共8个数字，9个单数的时候其实是一样的
      * @return
      */
     public static int maxSubSum3(int[] a, int left, int right) {
@@ -92,11 +94,7 @@ public class MaxSumFromContinuous {
         }
         int center = (left + right) / 2;
         int maxLeftSum = maxSubSum3(a, left, center);
-
-
         int maxRightSum = maxSubSum3(a, center + 1, right);
-
-
 
         int maxLeftBorderSum = 0, leftBorderSum = 0;
         for (int i = center; i >= left; i--) {
@@ -113,38 +111,70 @@ public class MaxSumFromContinuous {
             }
         }
         int maxBorderSum = maxLeftBorderSum + maxRightBorderSum;
-        return maxBorderSum > maxLeftSum ? maxBorderSum > maxRightSum ? maxBorderSum : maxRightSum
-                : maxLeftSum > maxRightSum ? maxLeftSum : maxRightSum;
+        return maxBorderSum > maxLeftSum ? (maxBorderSum > maxRightSum ? maxBorderSum : maxRightSum)
+                : (maxLeftSum > maxRightSum ? maxLeftSum : maxRightSum);
     }
 
     /**
      * 算法思想：设a[i]为和最大序列的起点，则如果a[i]是负的，那么它不可能代表最优序列的起点，因为任何包含a[i]作为起点的子序列都可以通过a[i+1]作起点而得到改进。
      * 类似的，任何负的子序列也不可能是最优子序列的前缀。
+     * 核心思想就是：这段序列头部肯定是正数，如果相邻相加还是负数，那就可以舍弃了。
      */
 
     public static int maxSubSum4(int[] a){
-
         int maxSum=0,sum=0;
-
         for(int i=0;i<a.length;i++){
-
             sum+=a[i];
-
             if(sum>maxSum){
-
                 maxSum=sum;
-
             }else if(sum<0){
-
                 sum=0;
-
             }
-
         }
-
         return maxSum;
-
     }
 
+    //分治算法-读不懂，背下来
+    public static int divideAndConque(int[] arr,int left,int right){
+        //递归终止的条件-当分组到最终的单个元素时
+        if(left == right){
+            if(left > 0){
+                return arr[left];
+            }else{
+                return 0;
+            }
+        }
+        int center = (left+right)/2;//求分界点
+        //进行分组时，求左边分组最大值
+        int leftMax = divideAndConque(arr,left,center);
+
+        //进行分组时，求右边分组最大值
+        int rightMax = divideAndConque(arr,center+1,right);
+
+        //进行边界最大值和
+        int maxBorderSum = 0;
+        //左边界最大值和
+        int leftBorderMaxSum = 0;
+        int leftBorderSum = 0;
+        for (int i = center; i >= left ; i--) {
+            leftBorderSum += arr[i];
+            if(leftBorderSum > leftBorderMaxSum){
+                leftBorderMaxSum = leftBorderSum;
+            }
+        }
+
+        //右边界最大和求值
+        int rightBorderMaxSum = 0;
+        int rightBorderSum = 0;
+        for (int i = center+1; i <= right; i++) {
+            rightBorderSum += arr[i];
+            if(rightBorderSum > rightBorderMaxSum){
+                rightBorderMaxSum = rightBorderSum;
+            }
+        }
+        maxBorderSum = leftBorderMaxSum + rightBorderMaxSum;
+        return maxBorderSum>leftMax?(maxBorderSum > rightMax? maxBorderSum:rightMax):(leftMax > rightMax? leftMax:rightMax);
+
+    }
 
 }
