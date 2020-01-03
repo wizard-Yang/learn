b0cca559864cfd4e15a455c7ccbd47f37dc09227
xQuantComputer <511574545@qq.com>
2020-01-03 08:51:03 +0800

    2019-1-3数组不初始化直接赋值报错

---
+++ src/main/java/com/sjyang/数组/数组不初始化直接赋值/ValueArray.java
@@ -0,0 +1,14 @@
+package com.sjyang.数组.数组不初始化直接赋值;
+
+/**
+ * @author saijun.yang
+ * @date Created in 2020/1/3 8:48
+ * @description 数组直接赋值
+ */
+public class ValueArray {
+    public static void main(String[] args) {
+        int a[] = null;
+        a[0] = 1;
+        System.out.println(a.length);
+    }
+}
