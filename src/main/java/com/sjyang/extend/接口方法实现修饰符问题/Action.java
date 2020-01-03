3757ba8141f6abfac0ed9982b327b86b6c77c632
xQuantComputer <511574545@qq.com>
2020-01-03 08:43:21 +0800

    2019-1-3接口方法修饰符

---
+++ src/main/java/com/sjyang/extend/接口方法实现修饰符问题/Action.java
@@ -0,0 +1,8 @@
+package com.sjyang.extend.接口方法实现修饰符问题;
+
+/**
+ * 接口中方法默认修饰符就是public abstract(如果你不写的话)
+ */
+public interface Action {
+    public abstract void fly();
+}
---
+++ src/main/java/com/sjyang/extend/接口方法实现修饰符问题/Hero.java
@@ -0,0 +1,13 @@
+package com.sjyang.extend.接口方法实现修饰符问题;
+
+/**
+ * @author saijun.yang
+ * @date Created in 2020/1/3 8:36
+ * @description
+ */
+public class Hero implements Action{
+    @Override
+    public void fly() {
+
+    }
+}
