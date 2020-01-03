4d7627fb57e7058d09edc8787e64e902ee648a43
xQuantComputer <511574545@qq.com>
2020-01-03 09:11:58 +0800

    2019-1-3设置执行参数，无实际意义，只是展示数组越界异常

--- src/main/java/com/sjyang/GitTest.java
+++ src/main/java/com/sjyang/GitTest.java
@@ -4,12 +4,13 @@ package com.sjyang;
  * @author ：saijun.yang
  * @date ：Created in 2019/4/9 17:33
  * @description：测试Git版本控制
- * @modified By：
- * @version:
+ * @modified By：java.lang.ArrayIndexOutOfBoundsException: 0
+ * @version: 设置执行参数
  */
 public class GitTest {
     public static void main(String[] args) {
-        int a = 9>5?(9>4?9:4):(5>4?5:4);
-        System.out.println(a);
+       String a = args[0];
+       String b = args[1];
+        System.out.println(args.length);
     }
 }
